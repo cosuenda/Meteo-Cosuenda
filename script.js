266379faee3c773b7a4b8559b837d96ebf6ff241
cosuenda <88199562+cosuenda@users.noreply.github.com>
2026-02-16 23:29:36 +0100

    Refactor API data handling and logging
    Updated console log message for better clarity. Removed unused code for processing API response.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const appKey = "26C4D6AD21CF8F8C4F3BA85E1CAF6701";
 const apiKey = "adf65434-1ace-43dd-b9a9-27915843d243";
 const mac = "84:CC:A8:B4:B1:F6";
 
-// ✅ URL API REAL (no la web)
 const url = `https://api.ecowitt.net/api/v3/device/real_time?application_key=${appKey}&api_key=${apiKey}&mac=${mac}&call_back=all`;
 
 async function obtenerDatos() {
@@ -10,20 +9,7 @@ async function obtenerDatos() {
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(JSON.stringify(data, null, 2));
-
-
-    if (data.code === 0) {
-      const outdoor = data.data.outdoor;
-
-      document.getElementById("temp").textContent =
-        outdoor.temperature.value + " °C";
-
-      document.getElementById("hum").textContent =
-        outdoor.humidity.value + " %";
-    } else {
-      console.error("Error API:", data);
-    }
+    console.log("Datos recibidos (JSON completo):", JSON.stringify(data, null, 2));
 
   } catch (error) {
     console.error("Error de conexión:", error);
@@ -31,6 +17,9 @@ async function obtenerDatos() {
 }
 
 obtenerDatos();
+
+
+
 
 
 
