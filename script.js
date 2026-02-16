08247ab2f5836cdfcf6bc1e78baf951196a535ed
cosuenda <88199562+cosuenda@users.noreply.github.com>
2026-02-16 23:12:59 +0100

    Update API URL for fetching real-time data

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const appKey = "26C4D6AD21CF8F8C4F3BA85E1CAF6701";
 const apiKey = "adf65434-1ace-43dd-b9a9-27915843d243";
 const mac = "84:CC:A8:B4:B1:F6";
 
-const url = `https://www.ecowitt.net/home/index?id=61227${appKey}&api_key=${apiKey}&mac=${mac}`;
+const url = `https://api.ecowitt.net/api/v3/device/real_time=${appKey}&api_key=${apiKey}&mac=${mac}`;
 
 fetch(url)
   .then(res => res.json())
