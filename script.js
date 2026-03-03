645d70ff9d9f63b089565de3fead83fb709c686d
cosuenda <88199562+cosuenda@users.noreply.github.com>
2026-03-03 10:54:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -49,6 +49,8 @@ async function obtenerDatos(){
         const rainMm = inToMm(rain.daily.value);
         const rainMonthMm = inToMm(rain.month?.value ?? 0);
         const pressHpa = inHgToHpa(p.relative.value);
+        
+        // RADIACIÓN Y UV (corrección por estructura de JSON)
         const uvIndex = data.data.solar_and_uvi?.uvi?.value ?? "--";
         const solar = data.data.solar_and_uvi?.solar?.value ?? "--";
 
